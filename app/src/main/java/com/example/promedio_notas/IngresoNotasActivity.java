fccba9097db938ed460cd8ef8190e4a64f750e69
PlayerC26 <yinmorales@uniboyaca.edu.co>
2022-10-11 15:32:00 -0500

    Primer commit de interfaces CG

---
+++ app/src/main/java/com/example/promedio_notas/IngresoNotasActivity.java
@@ -0,0 +1,14 @@
+package com.example.promedio_notas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IngresoNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingreso_notas);
+    }
+}
